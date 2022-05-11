5682fd7f7229bccb729ab0a022976c8efeeeb9d4
ushknn <uekiworks@gmail.com>
2022-05-11 13:09:01 +0900

    ch 4.4.2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,15 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true
   validates :name, length: {maximum: 30}
   validate :validate_name_not_include_comma
 
   private
 
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
   def validate_name_not_include_comma
     error.add(:name, 'にカンマを含めることはできません') if name&.include?('.')
   end
